97a87641de49b7e62c2c7bca18c9861d23cf7781
Harshit @193 <166112242+098HS@users.noreply.github.com>
2026-06-07 12:51:32 +0530

    Create Client.java

---
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -0,0 +1,98 @@
+package com.wellsfargo.counselor.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String email;
+
+    @Column
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "advisor_id")
+    private FinancialAdvisor financialAdvisor;
+
+    @OneToOne(mappedBy = "client")
+    private Portfolio portfolio;
+
+    public Client(String firstName, String lastName,
+                  String email, String phone,
+                  FinancialAdvisor financialAdvisor,
+                  Portfolio portfolio) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.financialAdvisor = financialAdvisor;
+        this.portfolio = portfolio;
+    }
+
+    public Client() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public FinancialAdvisor getFinancialAdvisor() {
+        return financialAdvisor;
+    }
+
+    public void setFinancialAdvisor(FinancialAdvisor financialAdvisor) {
+        this.financialAdvisor = financialAdvisor;
+    }
+
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+}
